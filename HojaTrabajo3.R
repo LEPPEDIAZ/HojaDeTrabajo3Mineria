f8416aafd7c07a2aa7e52701335da58a113047f5
Unknown <dia151378@uvg.edu.gt>
2019-03-06 22:56:32 -0600

    data reading

---
+++ HojaTrabajo3.R
@@ -0,0 +1,6 @@
+perros <- read.csv("breed_labels.csv")
+perros2 <- read.csv("color_labels.csv")
+perros3 <- read.csv("state_labels.csv")
+#escogeremos la data de sample_submissions
+perros4 <- read.csv("sample_submission.csv")
+perros5 <- read.csv("train.csv")
